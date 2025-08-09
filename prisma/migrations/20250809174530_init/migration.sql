fa293639702d88e570e27c98eb8d40788b085161
gbl <saldanha@gbldev.com>
2025-08-09 14:46:31 -0300

    Ajustando db e questões

---
+++ prisma/migrations/20250809174530_init/migration.sql
@@ -0,0 +1,70 @@
+-- CreateEnum
+CREATE TYPE "public"."PagamentoStatus" AS ENUM ('PENDENTE', 'CONFIRMADO');
+
+-- CreateEnum
+CREATE TYPE "public"."SorteioStatus" AS ENUM ('AGUARDANDO', 'REALIZADO', 'CANCELADO');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizadoEm" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Participante" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "email" TEXT,
+    "aceitouLGPD" BOOLEAN NOT NULL,
+    "numero" INTEGER NOT NULL,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "pagamentoId" TEXT,
+
+    CONSTRAINT "Participante_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Pagamento" (
+    "id" TEXT NOT NULL,
+    "participanteId" TEXT NOT NULL,
+    "valor" DOUBLE PRECISION NOT NULL,
+    "status" "public"."PagamentoStatus" NOT NULL DEFAULT 'PENDENTE',
+    "metodo" TEXT NOT NULL,
+    "confirmadoEm" TIMESTAMP(3),
+
+    CONSTRAINT "Pagamento_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Sorteio" (
+    "id" TEXT NOT NULL,
+    "titulo" TEXT NOT NULL,
+    "descricao" TEXT,
+    "dataSorteio" TIMESTAMP(3) NOT NULL,
+    "status" "public"."SorteioStatus" NOT NULL DEFAULT 'AGUARDANDO',
+    "numeroVencedor" INTEGER,
+
+    CONSTRAINT "Sorteio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Participante_numero_key" ON "public"."Participante"("numero");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Participante_pagamentoId_key" ON "public"."Participante"("pagamentoId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Pagamento_participanteId_key" ON "public"."Pagamento"("participanteId");
+
+-- AddForeignKey
+ALTER TABLE "public"."Participante" ADD CONSTRAINT "Participante_pagamentoId_fkey" FOREIGN KEY ("pagamentoId") REFERENCES "public"."Pagamento"("id") ON DELETE SET NULL ON UPDATE CASCADE;
