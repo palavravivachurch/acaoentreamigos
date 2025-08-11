20e2f515c17f99e86ce7567781244ff867eac896
gbl <saldanha@gbldev.com>
2025-08-11 16:48:36 -0300

    Criando Config Docker

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-import type {NextConfig} from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-    /* config options here */
-};
+import {FlatCompat} from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+    // import.meta.dirname is available after Node.js v20.11.0
+    baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+    ...compat.config({
+        extends: ['next', 'prettier'],
+    }),
+]
 
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+export default withNextIntl(eslintConfig);
