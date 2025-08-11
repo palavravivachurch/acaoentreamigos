7deb5bc9a86a75eba64061ad70a7874d0a4961bf
gbl <saldanha@gbldev.com>
2025-08-11 18:00:40 -0300

    Prettier

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import {NextConfig} from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+    output: 'standalone',
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
