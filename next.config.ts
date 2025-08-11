9e075e628690ccf458705f4bb6deee7729895c8b
gbl <saldanha@gbldev.com>
2025-08-11 17:55:33 -0300

    Prettier

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ import {NextConfig} from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-    experimental: {
-        // @ts-ignore
-        missingSuspenseWithCSRBailout: false
-    },
-    output: 'standalone',
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
